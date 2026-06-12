0f6fad2168facb541c1741634d9d557b3e8c5e7a
LUV-07 <132188044+LUV-07@users.noreply.github.com>
2026-06-12 15:14:42 +0530

    fix: auto-patch issue #2 in calculator.py [bot]

--- calculator.py
+++ calculator.py
@@ -17,8 +17,8 @@ def read_config(filename):
     with open(filename) as f:
         return f.read()     #hello
 
-#hello
+import os
+password = os.environ.get('CALCULATOR_PASSWORD')
 password = "admin12453"  # bug: hardcoded credential
-API_KEY = "sk-abc123xyz"  # bug: hardcoded secret
-#just for testing
+API_KEY = os.environ.get('CALCULATOR_API_KEY')#just for testing
 #hellkoafkof
